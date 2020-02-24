06543c26dd45abd746c71215d185631241b46e23
Freddy Munive <munivef@yahoo.com>
2020-02-23 21:12:56 -0500

    adding a ngmodel, form and service with firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeroesComponent } from './pages/heroes/heroes.component';
-import { HeroeComponent } from './pages/heroe/heroe.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeroesComponent } from "./pages/heroes/heroes.component";
+import { HeroeComponent } from "./pages/heroe/heroe.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent,
-    HeroeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeroesComponent, HeroeComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { HeroesModel } from "src/app/models/heroes.model";
 
 @Component({
-  selector: 'app-heroe',
-  templateUrl: './heroe.component.html',
+  selector: "app-heroe",
+  templateUrl: "./heroe.component.html",
   styles: []
 })
 export class HeroeComponent implements OnInit {
+  heroe: HeroesModel = new HeroesModel();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  guardarInfo(form: NgForm) {
+    if (form.invalid) {
+      console.log("form no  válido");
+      return;
+    }
+    console.log("form válido");
   }
-
 }

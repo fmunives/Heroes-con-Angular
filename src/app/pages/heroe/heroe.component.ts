9816210b319a9c38d3097a7bbfb16016cd8ccd04
Freddy Munive <munivef@yahoo.com>
2020-02-23 21:38:05 -0500

    adding a creation of heros and ngmodel of form

--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { HeroesModel } from "src/app/models/heroes.model";
+import { FirebaseService } from "src/app/services/firebase.service";
+
+import Swal from "sweetalert2";
+import { Observable } from "rxjs";
 
 @Component({
   selector: "app-heroe",
@@ -9,7 +13,7 @@ import { HeroesModel } from "src/app/models/heroes.model";
 })
 export class HeroeComponent implements OnInit {
   heroe: HeroesModel = new HeroesModel();
-  constructor() {}
+  constructor(private firebase: FirebaseService) {}
 
   ngOnInit(): void {}
 
@@ -18,6 +22,31 @@ export class HeroeComponent implements OnInit {
       console.log("form no  válido");
       return;
     }
+
+    Swal.fire({
+      title: "espere por favor",
+      text: "guardando información",
+      icon: "info"
+    });
+    Swal.showLoading();
+
+    let posicion: Observable<any>;
+
+    if (this.heroe.id) {
+      posicion = this.firebase.updateHero(this.heroe);
+    } else {
+      posicion = this.firebase.createHero(this.heroe);
+    }
+
+    posicion.subscribe(resp => {
+      Swal.fire({
+        title: `${this.heroe.nombre}`,
+        text: "actualización correcta",
+        icon: "success"
+      });
+    });
+
     console.log("form válido");
+    console.log(this.heroe);
   }
 }
--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,9 +1,44 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { HeroesModel } from "../models/heroes.model";
+
+import { map } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class FirebaseService {
+  url = "https://login-angular-9714d.firebaseio.com";
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  createHero(heroe: HeroesModel) {
+    return this.http.post(`${this.url}/heros.json`, heroe).pipe(
+      map((resp: any) => {
+        heroe.id = resp.name;
+        return heroe;
+      })
+    );
+  }
+
+  updateHero(heroe: HeroesModel) {
+    const heroeTemp = {
+      ...heroe
+    };
+
+    delete heroeTemp.id;
+    return this.http.put(`${this.url}/heros/${heroe.id}.json`, heroeTemp);
+  }
+
+  getHeros() {
+    return this.http.get(`${this.url}/heros.json`);
+  }
+
+  getHero(id: string) {
+    return this.http.get(`${this.url}/heros/${id}.json`);
+  }
+
+  deleteHero(id: string) {
+    return this.http.delete(`${this.url}/heros/${id}.json`);
+  }
 }
